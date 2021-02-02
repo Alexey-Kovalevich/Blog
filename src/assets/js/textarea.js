3dd4d7bb549f25df5ebc130ff0756647b8871d4e
Alexey <belshina25@gmail.com>
2021-02-02 16:54:57 +0300

    feat: add mobileNav.js, modal.js, textarea.js files

---
+++ src/assets/js/textarea.js
@@ -0,0 +1,11 @@
+const textArea = document.querySelectorAll('[data-autoresize]');
+
+textArea.forEach(item => {
+  let textAreaH = item.offsetHeight;
+  item.addEventListener('input', event => {
+    let target = event.target;
+
+    target.style.height = textAreaH + 'px';
+    target.style.height = target.scrollHeight + 'px';
+  });
+});
